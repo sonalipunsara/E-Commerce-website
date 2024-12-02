a3215bf96c446354047bfd8278afbdcadfba7e48
sonalipunsara <sonalipunsara@gmail.com>
2024-12-02 09:18:20 +0530

    Link the pages with Navigation bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,27 +2,47 @@ import React from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="nav-logo">
-        <img src={logo} alt=""></img>
+        <img src={logo} alt="Logo" />
         <p>SHOPPER</p>
       </div>
 
       <ul className="nav-menu">
         <li>
-          Shop <hr />
+          <Link style={{ textDecoration: "none" }} to="/">
+            Home
+          </Link>
         </li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li>
+          <Link style={{ textDecoration: "none" }} to="/mens">
+            Men
+          </Link>
+        </li>
+        <li>
+          <Link style={{ textDecoration: "none" }} to="/womens">
+            Women
+          </Link>
+        </li>
+        <li>
+          <Link style={{ textDecoration: "none" }} to="/kids">
+            Kids
+          </Link>
+        </li>
       </ul>
 
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt=""></img>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+
+        <Link to="/cart">
+          <img src={cart_icon} alt="Cart Icon" />
+        </Link>
 
         <div className="nav-cart-count">0</div>
       </div>
