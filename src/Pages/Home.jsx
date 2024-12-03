bed9943202cc01e8d77a596610c6570a3d8211db
sonalipunsara <sonalipunsara@gmail.com>
2024-12-03 10:49:49 +0530

    New section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Popular from "../Components/Popular/Popular";
 import Offers from "../Components/Offers/Offers";
+import New from "../Components/New/New";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <Popular />
       <Offers />
+      <New />
     </div>
   );
 };
